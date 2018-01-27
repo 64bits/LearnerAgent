9131cff2eebeaa5869fc6cba6bb867c32d8e74ca
Rohit Garg <rohit@unity3d.com>
2018-01-26 20:02:52 -0800

    Adding in the .gitignore, working on setting up graph

--- Graph.cs
+++ Graph.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace LearnerAgent
@@ -7,9 +8,19 @@ namespace LearnerAgent
     {
         private List<Node> Nodes { get; set; }
 
+        private Node _pleasureNode;
+        private Node _painNode;
+        private Node[] _pleasureInputs;
+        private Node[] _painInputs;
+        private Node[] _moveOutputs;
+
         public Graph()
         {
             Nodes = new List<Node>();
+            
+            // Create a pleasure node and connect pleasure inputs with one degree of separation
+            
+            // Create a pin node and connect pain inputs with one degree of separation
         }
 
         public Node GetPleasureNode()
